b285e56c00e9c4aec32db63e04eb5721587b1cb4
CJ Voege <cjvqm9@mail.missouri.edu>
2014-12-02 13:34:43 -0600

    Created Developer Branch
    Title

--- FrontEnd/Dashboard/Dashboard/Dashboard/dashboardViewController.swift
+++ FrontEnd/Dashboard/Dashboard/Dashboard/dashboardViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class dashboardViewController: UIViewController {
 
+    //circle to be animated
+    let shape = CAShapeLayer()
+    let progress = CAShapeLayer()
+    
+    //function that converts degrees to radians
+    func DegreesToRadians (value:Float) -> Float {
+        return value * Float(M_PI) / 180.0
+    }
+    
+    @IBOutlet weak var avgStepView: UIView!
+   
+    @IBOutlet weak var circleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //CIRCLE!!!
+        // Customize the appearance of the shape layer
+        shape.fillColor = UIColor.clearColor().CGColor
+        shape.strokeColor = view.tintColor.CGColor
+        shape.lineWidth = 5.0
+        
+        // Make a rect to draw our shape in
+        let rect = CGRectMake(0.0, 0.0, 100.0, 100.0)
+        
+        // Set the path for the shape layer
+        shape.path = UIBezierPath(ovalInRect: rect).CGPath
+        
+        // Add the shape layer as a sub layer of our view
+        circleView.layer.addSublayer(shape)
+        
+        
+        // Inner arc
+        progress.fillColor = UIColor.clearColor().CGColor
+        progress.strokeColor = UIColor.orangeColor().CGColor
+        progress.lineWidth = shape.lineWidth
+        progress.path = UIBezierPath(ovalInRect: rect).CGPath
+        view.layer.addSublayer(progress)
+        
+        // Animate the shape change
+        let duration = NSNumber(float: 2.0)
+        var newAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        newAnimation.fromValue = NSNumber(float: 0.0)
+        newAnimation.toValue = NSNumber(float:1.0)
+        newAnimation.duration = 4.0
+        newAnimation.delegate = self
+        newAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        progress.addAnimation(newAnimation, forKey: "strokeEnd Animation")
+        
         // Do any additional setup after loading the view.
     }
 
