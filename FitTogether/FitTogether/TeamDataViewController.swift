edc82ce607e5164fa5ac9f529a974a114aafb7c1
CJ Voege <cjvqm9@mail.missouri.edu>
2014-12-09 11:58:42 -0600

    Revert 5eca3bc..a249ecf
    This rolls back to commit 5eca3bc7649f16cfb95c9705051ca18b3388b614.

--- FitTogether/FitTogether/TeamDataViewController.swift
+++ FitTogether/FitTogether/TeamDataViewController.swift
@@ -13,13 +13,20 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var teamNameLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
-    var fakeData = ["Bart", "Adam", "Chris", "Dave", "Justin", "Scott", "Pete", "Matt", "Tyler", "Aaron", "Jill", "Jack", "Frank", "Tim"]
+    var teamData = TeamData()
+    
+    //var fakeData = TeamData.passArrayOfData()
+ /*   var fakeData = ["Bart", "Adam", "Chris", "Dave", "Justin", "Scott", "Pete", "Matt", "Tyler", "Aaron", "Jill", "Jack", "Frank", "Tim"]
     
     var fakeSteps = ["2000", "3000", "1000", "5000", "4000", "9000", "8000", "9000", "0", "11000", "7000", "4000", "6000", "4000"]
     
-    
+  */
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var ckHelper = CloudKitHelper()
+        var userArray: AnyObject = ckHelper.retriveRecords("ID", queryRecordType: "Team")
+        NSLog("Elements in array: \(userArray)")
         // Do any additional setup after loading the view.
     }
     
@@ -33,18 +40,22 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fakeData.count
+        //return fakeData.count
+        //return teamData.userData.count
+        return 1
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        cell.textLabel.text = self.fakeData[indexPath.row]
+        
+     //   cell.textLabel.text = self.fakeData[indexPath.row]
+       //cell.textLabel.text = self.teamData.userData[indexPath.row]
         
        // var imageName = UIImage(named: fakeData[indexPath.row])
         var imageName = UIImage(named: "theDude.png")
-        cell.imageView.image = imageName
+        //cell.imageView.image = imageName
         
-        cell.detailTextLabel?.text = self.fakeSteps[indexPath.row]
+     //   cell.detailTextLabel?.text = self.fakeSteps[indexPath.row]
         
         return cell
     }
@@ -63,8 +74,8 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
         case 0:
             //List users by A - Z
             //same view with data loaded differently
-            var sortedFakeData = fakeData.sorted { $0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending}
-            fakeData = sortedFakeData
+            //var sortedFakeData = fakeData.sorted { $0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending}
+            //fakeData = sortedFakeData
             self.tableView.reloadData()
             
         case 1:
