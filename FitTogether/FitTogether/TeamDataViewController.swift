454d58b0941a5d41d22f5dbb10caa94ce63683cc
Justin Schubring <jbschubring@gmail.com>
2014-12-09 11:29:00 -0600

    moved data to model. added high/low sort function
    moved data to model. added high/low sort functionality

--- FitTogether/FitTogether/TeamDataViewController.swift
+++ FitTogether/FitTogether/TeamDataViewController.swift
@@ -14,16 +14,11 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var tableView: UITableView!
     
     var teamData = TeamData()
-    
-    var fakeData = TeamData.passArrayOfData()
- /*   var fakeData = ["Bart", "Adam", "Chris", "Dave", "Justin", "Scott", "Pete", "Matt", "Tyler", "Aaron", "Jill", "Jack", "Frank", "Tim"]
-    
-    var fakeSteps = ["2000", "3000", "1000", "5000", "4000", "9000", "8000", "9000", "0", "11000", "7000", "4000", "6000", "4000"]
-    
-  */
+    var fakeData:[(name: String, amtOfStepsWalked: String)]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        fakeData = teamData.passArrayOfData()
         var ckHelper = CloudKitHelper()
         var userArray: AnyObject = ckHelper.retriveRecords("ID", queryRecordType: "Team")
         NSLog("Elements in array: \(userArray)")
@@ -41,20 +36,20 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return fakeData.count
-        return teamData.userData.count
+        return fakeData!.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         
-     //   cell.textLabel.text = self.fakeData[indexPath.row]
-       //cell.textLabel.text = self.teamData.userData[indexPath.row]
+        let item = self.fakeData![indexPath.row]
+        cell.textLabel.text = item.name
+        cell.detailTextLabel?.text = item.amtOfStepsWalked
         
-       // var imageName = UIImage(named: fakeData[indexPath.row])
+        // var imageName = UIImage(named: fakeData[indexPath.row])
         var imageName = UIImage(named: "theDude.png")
         cell.imageView.image = imageName
-        
-     //   cell.detailTextLabel?.text = self.fakeSteps[indexPath.row]
+
         
         return cell
     }
@@ -72,20 +67,16 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
         {
         case 0:
             //List users by A - Z
-            //same view with data loaded differently
-            var sortedFakeData = fakeData.sorted { $0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending}
-            fakeData = sortedFakeData
+            fakeData = teamData.AtoZ()
             self.tableView.reloadData()
             
         case 1:
             //List users by Most Steps
-            //Call model?
-            //load table view here?
+            fakeData = teamData.HighToLow()
             tableView.reloadData()
         case 2:
             //List users by Least Steps
-            //Call model?
-            //load table view here
+            fakeData = teamData.LowToHigh()
             tableView.reloadData()
         default:
             break
