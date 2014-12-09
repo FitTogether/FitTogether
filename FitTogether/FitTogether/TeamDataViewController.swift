581ac64191b7202c6cd81bf2e8918c1bc3e49471
Matt Whitesides <mbwxd4@mail.missouri.edu>
2014-12-09 12:13:28 -0600

    fixed xib

--- FitTogether/FitTogether/TeamDataViewController.swift
+++ FitTogether/FitTogether/TeamDataViewController.swift
@@ -43,12 +43,12 @@ class TeamDataViewController: UIViewController, UITableViewDelegate, UITableView
         var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         
         let item = self.fakeData![indexPath.row]
-        cell.textLabel.text = item.name
+        cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = item.amtOfStepsWalked
         
         // var imageName = UIImage(named: fakeData[indexPath.row])
         var imageName = UIImage(named: "theDude.png")
-        cell.imageView.image = imageName
+        cell.imageView?.image = imageName
         
         
         return cell
